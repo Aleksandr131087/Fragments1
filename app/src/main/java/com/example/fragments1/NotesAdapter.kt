c2965d299753a08cc22e5250bf4a2e1403f3a627
Aleksandr131087 <an.petrov13@gmail.com>
2024-12-22 17:06:49 +0400

    порядковый номер

--- app/src/main/java/com/example/fragments1/NotesAdapter.kt
+++ app/src/main/java/com/example/fragments1/NotesAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 class NotesAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteId: TextView = itemView.findViewById(R.id.note_id)
         val noteText: TextView = itemView.findViewById(R.id.note_text)
         val noteCheckbox: CheckBox = itemView.findViewById(R.id.note_checkbox)
         val noteTimestamp: TextView = itemView.findViewById(R.id.note_timestamp)
@@ -22,6 +23,7 @@ class NotesAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NotesAd
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val note = notes[position]
+        holder.noteId.text = note.id.toString()
         holder.noteText.text = note.text
         holder.noteCheckbox.isChecked = note.isChecked
         holder.noteTimestamp.text = note.timestamp
